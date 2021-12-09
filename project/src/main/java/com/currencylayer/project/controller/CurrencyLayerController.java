84386d3327e4ae234ab13803a193bd4a80d68f0d
sarabrusch <s1095512@studenti.univpm.it>
2021-12-09 19:42:39 +0100

    creazione Controller
    Definizione prime rotte
    
    Co-Authored-By: Marco Di Vita <93443343+marcopapero@users.noreply.github.com>

---
+++ project/src/main/java/com/currencylayer/project/controller/CurrencyLayerController.java
@@ -0,0 +1,37 @@
+package com.currencylayer.project.controller;
+
+import java.util.*;
+import com.currencylayer.project.service.*;
+
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyLayerController {
+
+	@Autowired
+	private CurrencyLayerServiceImpl currencyLayerService;
+	
+	@RequestMapping(value = "/live")
+	public ResponseEntity<Object> getLiveQuotation() throws ParseException {
+		return new ResponseEntity<>(currencyLayerService.getLiveOrList("live"),HttpStatus.OK);
+	} 
+	
+	@RequestMapping(value = "/list")
+	public ResponseEntity<Object> getCurrencyList() throws ParseException {
+		return new ResponseEntity<>(currencyLayerService.getLiveOrList("list"),HttpStatus.OK);
+	} 
+	
+	@RequestMapping(value = "/historical/{date}")
+	public ResponseEntity<Object> getHistory(Map<String,Object> model,@PathVariable String date) throws ParseException {
+		model.put("date", date);
+		return new ResponseEntity<>(currencyLayerService.getHistoricalQuotation("historical",date),HttpStatus.OK);
+	}
+	
+	
+}
